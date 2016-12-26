6a6c7542e710691a9107b9b4ae3a92a32f5026fa
onno204 <restafval@hotmail.nl>
2016-12-26 23:32:29 +0100

    13

--- CodingParts/src/Parts/DeathCounter.java
+++ CodingParts/src/Parts/DeathCounter.java
@@ -1,9 +1,14 @@
 package Parts;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -25,6 +30,28 @@ public class DeathCounter extends JavaPlugin implements Listener{
 		try{
 			Top = (HashMap<UUID, Integer>) o;
 		}catch(Exception e){ e.printStackTrace(); }
+	}
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if(command.getLabel().equalsIgnoreCase("KillTop")){
+			Object[] a = Top.entrySet().toArray();
+			Arrays.sort(a, new Comparator() {
+				public int compare(Object o1, Object o2) {
+			        return ((Map.Entry<String, Integer>) o2).getValue()
+			                   .compareTo(((Map.Entry<String, Integer>) o1).getValue());
+			    }
+			});
+			int Counting = 1;
+			for (Object e : a) {
+			    sender.sendMessage(Counting + ". " + ((Map.Entry<String, Integer>) e).getKey() + " : " + ((Map.Entry<String, Integer>) e).getValue());
+			    Counting++;
+			}
+			
+			
+		}
+		return true;
 	}
 	
 	@Override
