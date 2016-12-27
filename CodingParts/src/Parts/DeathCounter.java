e06aa00f57e7535aa36b2a0ec83c8632938ec789
onno204 <restafval@hotmail.nl>
2016-12-27 01:07:50 +0100

    Updated

--- CodingParts/src/Parts/DeathCounter.java
+++ CodingParts/src/Parts/DeathCounter.java
@@ -4,37 +4,42 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DeathCounter extends JavaPlugin implements Listener{
 	public static HashMap<UUID, Integer> Top = new HashMap<UUID, Integer>();
 	
-	
-	@SuppressWarnings("unchecked")
 	@Override
 	public void onEnable() {
 		System.out.println("Death Top Enabled");
 		Bukkit.getPluginManager().registerEvents(this, this);
 		if(!this.getDataFolder().exists()){ this.getDataFolder().mkdir(); }
 		reloadConfig();
-		Object o = getConfig().get("TopKills");
-		try{
-			Top = (HashMap<UUID, Integer>) o;
-		}catch(Exception e){ e.printStackTrace(); }
-		if(Top == null){
-			Top = new HashMap<UUID, Integer>();
+		Set<String> lst = getConfig().getKeys(true);
+		for(String s : lst){
+			Top.put(UUID.fromString(s), getConfig().getInt(s));
 		}
 	}
-
+	
+	@Override
+	public void onDisable() {
+		System.out.println("Disabled");
+		for(UUID uuid : Top.keySet()){
+			getConfig().set(uuid.toString(), Top.get(uuid));
+		}
+		saveConfig();
+	}
+	
     @SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -43,14 +48,15 @@ public class DeathCounter extends JavaPlugin implements Listener{
 				Object[] a = Top.entrySet().toArray();
 				Arrays.sort(a, new Comparator() {
 					public int compare(Object o1, Object o2) {
-				        return ((Map.Entry<String, Integer>) o2).getValue()
-				                   .compareTo(((Map.Entry<String, Integer>) o1).getValue());
+				        return ((Map.Entry<UUID, Integer>) o2).getValue()
+				                   .compareTo(((Map.Entry<UUID, Integer>) o1).getValue());
 				    }
 				});
 				int Counting = 1;
 				for (Object e : a) {
-				    sender.sendMessage(Counting + ". " + ((Map.Entry<String, Integer>) e).getKey() + " : " + ((Map.Entry<String, Integer>) e).getValue());
-				    Counting++;
+					OfflinePlayer p = Bukkit.getOfflinePlayer(((Map.Entry<UUID, Integer>) e).getKey());
+				    sender.sendMessage(Counting + ". " + p.getName()  + " : " + ((Map.Entry<UUID, Integer>) e).getValue().toString() );
+				    Counting = Counting + 1;
 				}
 			}catch(Exception e){ sender.sendMessage("Er is iets fout gegaan tijdens deze command!"); e.printStackTrace(); }
 			
@@ -58,17 +64,11 @@ public class DeathCounter extends JavaPlugin implements Listener{
 		return true;
 	}
 	
-	@Override
-	public void onDisable() {
-		System.out.println("Disabled");
-		getConfig().set("TopKills", Top);
-		saveConfig();
-	}
-	
 	@EventHandler
-	public static void DeathEvent(PlayerDeathEvent e){
+	public static void DeathEvent(org.bukkit.event.entity.EntityDeathEvent e){
+		if(!(e.getEntity() instanceof Player)){ return; }
 		if(e.getEntity() == null){ return; }
-		Player p = e.getEntity().getKiller();
+		Player p = ((Player)e.getEntity()).getKiller();
 		if(p != null){
 			int last = 0;
 			if(Top.containsKey(p.getUniqueId())){
@@ -83,3 +83,9 @@ public class DeathCounter extends JavaPlugin implements Listener{
 	}
 	
 }
+
+
+	
+	
+
+
